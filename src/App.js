e6607b74c7298deb8408cd9258d25a77b33e4c44
mdshahjalal5 <muhommodshahjalal@gmail.com>
2023-02-11 17:19:03 +0600

    basic react application

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alhamdulillah everything working very nice</h2>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure><img src='https://i.ibb.co/FqWkMvh/download.jpg' alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
