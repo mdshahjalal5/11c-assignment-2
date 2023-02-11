b40013457e67e05a0a8aa89a803abc4569e6a11c
mdshahjalal5 <muhommodshahjalal@gmail.com>
2023-02-11 21:44:38 +0600

    primary navbar added

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -7,8 +7,8 @@ const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
+            {/* <Outlet></Outlet>
+            <Footer></Footer> */}
         </div>
     );
 };
